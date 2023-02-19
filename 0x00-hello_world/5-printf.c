05cb1a3ec33a5e4d66790a3fb7176476ec112c05
TheGreatestbug <izyspikes@gmail.com>
2023-02-19 00:39:59 +0000

    size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  *    *
  *     * print a string and return 0 then exits
  *      * Return: int (0)
- *       */
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main(void)
 {
-
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(char));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(int);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(long int);
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(float);
+	return (0);
 }
