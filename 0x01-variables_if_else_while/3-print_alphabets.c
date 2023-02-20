d99fde9d273a9da666569f47b07bdfb5b58cb893
TheGreatestbug <izyspikes@gmail.com>
2023-02-20 03:11:27 +0000

    putchar

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 
 /**
- *  * main - Entry point
- *   * Description: Print lower and upper alphabet on the same line
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * Description: Print lower and upper alphabet on the same line
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
