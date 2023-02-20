e0e6d378c799bffa91fed58396d149dc40c977ae
TheGreatestbug <izyspikes@gmail.com>
2023-02-20 02:44:38 +0000

    0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,26 +1,39 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/*
+ * more headers goes there 
+ *
+ * betty style doc for function main goes there
+ *
+ * main - Entry point
+ *
+ * Description: This prohram will assign a random number to n.
+ *
+ *Return: always 0 (Success)
+*/
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-/* your code goes there */
+/*
+ * your code goes there
+*/
 	if(n < 0)
 		{
 		printf("%d is negative\n", n);
-		}
-	if(n == 0)
+	}
+
+	else if(n == 0)
 		{
 		printf("%d is zero", n);
-		}
-	if(n > 0)
+	}
+
+	else
 		{
 		printf("%d is positive", n);
-		}
+	}
+	
 	return (0);
 }
