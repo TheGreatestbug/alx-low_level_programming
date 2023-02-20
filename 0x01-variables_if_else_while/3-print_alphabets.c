f4260cf19e820c714bc4ab5db0bb4c6a01c1f69e
TheGreatestbug <izyspikes@gmail.como>
2023-02-20 15:12:52 -0800

    sandbox

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -14,15 +14,15 @@ int main(void)
 	char Z = 'A';
 
 	while (z <= 'z')
-		{
-			putchar(z);
-				z++;
-		}
+	{
+		putchar(z);
+			z++;
+	}
 	while (Z <= 'Z')
-		{
-			putchar(Z);
-				Z++;
-		}
+	{
+		putchar(Z);
+			Z++;
+	}
 	putchar('\n');
 	return (0);
 }
