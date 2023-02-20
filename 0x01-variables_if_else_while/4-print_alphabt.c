066679967ac8196af914156b811a808bca24a0a3
TheGreatestbug <izyspikes@gmail.com>
2023-02-20 03:40:30 +0000

    5

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,7 +5,7 @@
  *
  * Description: Print lowercase letters except e and q
  *
- * always return 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: This progran prints numbers from 0 to 10
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		{
+			printf(num);
+		}
+}
