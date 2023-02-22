8a8ac607955e6cacee3deae735f723dfff6cdd9d
TheGreatestbug <izyspikes@gmail.com>
2023-02-22 02:20:11 +0000

    0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * REturn: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char letter = a;
+
+	for (letter = 'a'; letter =< 'z' letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+	return (0;)
+}
