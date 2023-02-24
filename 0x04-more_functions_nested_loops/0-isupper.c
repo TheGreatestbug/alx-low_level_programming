903ac51b408b4f5745016bec4129f3b1b47898da
TheGreatestbug <izyspikes@gmail.com>
2023-02-24 21:04:04 +0000

    omo x10

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code.
- *
+ * _isupper - check the code.
+ * @c: is what I dont know
+ * Daniel and Steph are good teachers
  * Return: Always 0.
  */
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
