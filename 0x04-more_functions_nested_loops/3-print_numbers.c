00f6ac9e8b90f79954775e05a12fbf6357a09ff4
TheGreatestbug <izyspikes@gmail.com>
2023-02-25 17:57:19 +0000

    3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -10,9 +10,9 @@ void print_numbers(void)
 {
 	int num;
 
-	for (num >= 48; num <= 57; num++)
+	for (num = 48; num <= 57; num++)
 	{
-		_putchar (num);
+		_putchar (num + '0');
 	}
 	_putchar ('\n');
 
