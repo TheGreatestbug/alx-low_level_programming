52be176289df9760249e6912434af6a53031d428
TheGreatestbug <izyspikes@gmail.com>
2023-02-25 18:33:15 +0000

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_most_numbers: Print numbers 
- * void : My head is void
+ * print_most_numbers: Print numbers
+ *
  * Return: void
  */
 
 void print_most_numbers(void)
 {
 	for (num = 0; num <= 9; num++)
-	{	
+	{
 		if (num != 2 && num != 4)
 		{
 			_putchar (num + '0');
