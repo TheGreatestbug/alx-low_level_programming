0050b5cdb3a108233437ecdca5f096ea18df0344
TheGreatestbug <izyspikes@gmail.com>
2023-02-26 14:59:56 +0000

    5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,6 +5,7 @@
  *
  * Return: void
  */
+
 void more_numbers(void)
 {
 	int prt;
@@ -12,22 +13,22 @@ void more_numbers(void)
 	int first_digit;
 	int second_digit;
 
-	first_digit = num / 10;
-	second_digit = num % 10;
-
 	for (prt = 0; prt <= 9; prt++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
 			if (num < 10)
 			{
-				_putchar (num + '0');
+				second_number = num;
 			}
 			else
 			{
-				_putchar (first_digit + '0');
-				_putchar (second_digit + '0');
+				first_digit = num /10;
+			 	second_digit = num % 10;
+				_putchar (first_number + '0');
 			}
+			_putchar (second_number + '0');
 		}
-	}	
+		_putchar ('\n');
+	}
 }
