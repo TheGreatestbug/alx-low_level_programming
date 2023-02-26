bb569ceeee8a6a33782854899e1ecfec3721bb62
TheGreatestbug <izyspikes@gmail.com>
2023-02-26 17:11:27 +0000

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - Print line
+ *
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	int line;
+
+	if (n > 0)
+	{
+		for (line = 0; line <= n; line++)
+		{
+			_putchar (95);
+		}
+		_putchar ('\n');
+	}
+}
