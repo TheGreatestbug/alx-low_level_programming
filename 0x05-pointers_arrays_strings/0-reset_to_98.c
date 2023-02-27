3f908b09986e7042fa3fd974e8d0d1f99956afe7
TheGreatestbug <izyspikes@gmail.com>
2023-02-27 16:02:28 +0000

    0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,19 +2,12 @@
 
 /**
  * reset_to_98 - A function that resets to 98
- *
+ * @n: a parameter for n
  * Return: void
  */
 
 void reset_to_98(int *n)
 {
-	int n = 402;
-	int *n;
-	*n = &n;
-
-	_putchar (n = %d + '0');
-
 	*n = 98;
-	_putchar (n = %d + '0');
-	_putchar ('\n');
+
 }
