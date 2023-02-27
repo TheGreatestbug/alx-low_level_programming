b2118bcb86f696584395e803d727fbd10be7333e
TheGreatestbug <izyspikes@gmail.com>
2023-02-27 15:58:24 +0000

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that resets to 98
+ *
+ * Return: void
+ */
+
+void reset_to_98(int *n)
+{
+	int n = 402;
+	int *n;
+	*n = &n;
+
+	_putchar (n = %d + '0');
+
+	*n = 98;
+	_putchar (n = %d + '0');
+	_putchar ('\n');
+}
