eb1bf9a83e453c36fd676bca3c9661d67f7cdf64
TheGreatestbug <izyspikes@gmail.com>
2023-02-27 22:32:37 +0000

    0

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - check the code
+ *
+ * Return: Always 0
+ */
+
+void _puts(char *str)
+{
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	
+	_putchar (str);
+	_putchar ('\n');
+}
