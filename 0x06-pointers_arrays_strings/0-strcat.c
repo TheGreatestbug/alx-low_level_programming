bcd6f52c2d9adbbf1d5bf21b51f384e9ba366fcc
TheGreatestbug <izyspikes@gmail.com>
2023-03-02 14:30:07 +0000

    0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strcat - concatenate
+ * Description: A program that concatenates strings
+ * @dest: string
+ * @src: string also
+ * Return: dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (j = 0; src[j] != '\0'; j++)
+	{
+		dest[i++] = src[j];
+	}
+	return (dest);
+}
