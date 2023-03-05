b89bbc5578cddaa122fb82062d40bf9bad4b9e3d
TheGreatestbug <izyspikes@gmail.com>
2023-03-05 01:03:53 +0000

    fibonacci

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,21 +1,19 @@
 #include "main.h"
-
 /**
- * print_most_numbers - function that prints the numbers, from 0 to 9
- *
- * Return: void
+ * print_most_numbers - prints most numbers
+ * Return: Always 0
  */
 
 void print_most_numbers(void)
 {
 	int i;
 
-
 	for (i = 0; i < 10; i++)
 	{
-		if (i == 2 || i == 4)
-			continue;
-		putchar('0' + i);
+		if (i != 2 && i != 4)
+		{
+			_putchar('0' + i);
+		}
 	}
-	putchar('\n');
+	_putchar('\n');
 }
