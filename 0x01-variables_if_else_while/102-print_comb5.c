2a86f78f5759205b75d0368c523c229c71d930ec
TheGreatestbug <izyspikes@gmail.com>
2023-03-05 13:12:09 +0000

    print comb

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,32 +13,32 @@ int main(void)
 
 	for (c = 48; c <= 57; c++)
 	{
-		for (i = 48; i <= 57; i++)
+	for (i = 48; i <= 57; i++)
+	{
+	for (k = 48; k <= 57; k++)
+	{
+		for (j = 48; j <= 57; j++)
 		{
-			for (k = 48; k <= 57; k++)
+			if (((k + j) > (c + i) &&  k >= c) || c < k)
 			{
-				for (j = 48; j <= 57; j++)
+				putchar(c);
+				putchar(i);
+				putchar(' ');
+				putchar(k);
+				putchar(j);
+				if (c + i + k + j == 227 && c == 57)
 				{
-					if (((k + j) > (c + i) &&  k >= c) || c < k)
-					{
-						putchar(c);
-						putchar(i);
-						putchar(' ');
-						putchar(k);
-						putchar(j);
-						if (c + i + k + j == 227 && c == 57)
-						{
-							break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
+					break;
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
 				}
 			}
 		}
+	}
+	}
 	}
 	putchar('\n');
 	return (0);
