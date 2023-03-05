2ebc371028cdca5b777aec0a217c0b0703b4bf6a
TheGreatestbug <izyspikes@gmail.com>
2023-03-05 01:16:39 +0000

    10

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+/**
+ *main - entry point
+ *Return: 0 always
+ */
+int main(void)
+{
+	int a = 100;
+	int i;
+
+	i = 1;
+	while (i <= a)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			if (i < a)
+				printf("Buzz ");
+			else
+				printf("Buzz");
+		}
+		else
+		{																								printf("%i ", i);
+		
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
