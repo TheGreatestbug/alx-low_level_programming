1975f84281051cade536724f917cd3df98949336
TheGreatestbug <izyspikes@gmail.com>
2023-03-05 13:08:44 +0000

    print comb

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: This program prints all the numbers of base 16 in lowercase
+ * Return: Always 0 (SUccess)
+ * Author: SteffieMD
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x < 9; x++)
+	{
+		for (y = x + 1; y < 10; y++)
+		{
+			putchar('0' + x);
+			putchar('0' + y);
+
+			if (x == 8 && y == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
