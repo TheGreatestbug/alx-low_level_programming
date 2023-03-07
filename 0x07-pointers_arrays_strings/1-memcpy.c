44f0478c7369fd416e39ea82ec1f52642f274d97
TheGreatestbug <izyspikes@gmail.com>
2023-03-06 17:28:44 -0800

    mkdir

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,14 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	char *copied;
+	char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
 
-	copied = memcpy(dest, src, n);
-
-	return (copied);
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }
 
