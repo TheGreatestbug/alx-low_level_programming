c1399dbaf5475fcd6fd58adadeef59e347d3c8bd
TheGreatestbug <izyspikes@gmail.com>
2023-03-06 17:12:30 -0800

    mkdir

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - Entry point
+ * Description: A program that fills memory with a constant byte
+ * @s: variable 
+ * @n another var
+ * @b: a var
+ * Return: void
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *result;
+
+	result = memset(s,b,n);
+
+	return (result);
+}
+
