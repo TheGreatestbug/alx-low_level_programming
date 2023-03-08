fbeb9c8166f9c40e2008f97c46b61e0082d21f9f
TheGreatestbug <izyspikes@gmail.com>
2023-03-08 23:44:38 +0000

    Puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,11 +3,14 @@
 /**
  * _puts_recursion - Recursions the recursables
  * Desciription: A progaram that prints a string
+ * @s: A pointer variable
  * Return: void
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')	
-		Printf (%c, *s);
-	_puts_recursion (*s + 1);
+	if (*s == '\0')
+	{
+		Printf("%c\n", *s);
+	}
+	_puts_recursion(*s + 1);
 }
