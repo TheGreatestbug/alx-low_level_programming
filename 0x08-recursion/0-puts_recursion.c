4e57be774b822399ce9fa48a1835101feac41924
TheGreatestbug <izyspikes@gmail.com>
2023-03-08 23:55:17 +0000

    Puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _puts_recursion - Recursions the recursables
@@ -6,6 +7,7 @@
  * @s: A pointer variable
  * Return: void
  */
+
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,6 +1,2 @@
-#include <stdio.h>
-#include <string.h>
-#include <math.h>
-
 void _puts_recursion(char *s);
 int _strlen_recursion(char *s);
