a3bd284a9680cddadfd19c9a3d9ec839b70a3414
TheGreatestbug <izyspikes@gmail.com>
2023-03-10 00:38:06 +0000

    Factorial

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,8 +9,6 @@
 
 int _sqrt_recursion(int n)
 {
-	int sqtRecursive(int start, int end, int m)
-{
 	long mid;
 
 	if (end >= start)
@@ -18,25 +16,26 @@ int _sqrt_recursion(int n)
 		mid = start + (end - start) / 2;
 		if (mid * mid == m)
 			return (mid);
-/* following binary search */
+		//following binary search 
 		if (mid * mid > m)
 			return (sqtRecursive(start, mid - 1, m));
 		if (mid * mid < m)
 			return (sqtRecursive(mid + 1, end, m));
 	}
 	return (-1);
-}
+
 /**
  * _sqrt_recursion - finds the natural square root of a number
  * @n: given number
  * Return: square root of n or -1
  **/
 	int _sqrt_recursion(int n)
-{
+	{
 	if (n < 0)
-		return (-1);
+	return (-1);
 	if (n == 0 || n == 1)
 		return (n);
 	return (sqtRecursive(2, n, n));
-} 
+	}
+
 }
